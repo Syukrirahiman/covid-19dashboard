5c4e948026b0be98c9a3c989a51d685e6ebf0836
Syukrirahiman <43146706+Syukrirahiman@users.noreply.github.com>
2020-12-07 21:26:25 +0800

    Update App.js
    Edit the design

--- covid-19dashboard/src/App.js
+++ covid-19dashboard/src/App.js
@@ -1,30 +1,40 @@
-import React, { Component } from "react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import 'assets/css/App.css';
 
-import theme from "theme";
+import theme from 'theme';
+import { withStyles } from '@material-ui/core/styles';
 import Navbar from 'view/Navbar'
 import StatisticView from 'view/StatisticView/StatisticView'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <StatisticView/>
-//     </div>
-//   );
-// }
-
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Router>
           <Navbar />
+          <div className={classes.main}>
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/covid-dashboard/" />} />
+              {/* <Route path="/covid-dashboard/:country" component={SearchCountryView} /> */}
+              <Route path="/statistic" component={StatisticView} />
+            </Switch>
+          </div>
         </Router>
       </ThemeProvider>
     )
   }
 }
 
-export default App;
+const styles = (theme) => ({
+  main: {
+    flex: 1,
+    padding: theme.spacing(6, 4),
+    background: '#eaeff1',
+    minHeight: '80vh',
+  }
+});
+
+export default withStyles(styles)(App);
