fd495774a28fb88624c47e9fcfc44a36c75c7dd2
Syukrirahiman <43146706+Syukrirahiman@users.noreply.github.com>
2020-12-07 21:26:59 +0800

    Added Statistic component

--- covid-19dashboard/src/components/Statistic/Statistic.js
+++ covid-19dashboard/src/components/Statistic/Statistic.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import axios from "axios";
-export default class Statistic extends Component {
+
+import { Grid, withStyles, InputLabel, MenuItem, Paper, FormControl, Select, Typography } from '@material-ui/core';
+
+
+class Statistic extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: {}
+            data: [],
+            selectedData: null,
         }
     }
 
@@ -22,18 +27,114 @@ export default class Statistic extends Component {
                 'x-rapidapi-host': 'covid-193.p.rapidapi.com'
             }
         };
-
-        axios.request(options).then(function (response) {
-            console.log(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
+        axios.request(options)
+            .then((response) => {
+                this.setState({
+                    data: response.data.response
+                })
+            }).catch(function (error) {
+                console.error(error);
+            });
     }
-    render() {
-        return (
-            <div>
 
-            </div>
+    handleChange = (event) => {
+        this.setState({
+            selectedData: event.target.value
+        })
+    }
+
+    render() {
+        const { classes } = this.props;
+        console.log(this.state.selectedData)
+        return (
+            <React.Fragment>
+                <div>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Country</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={this.state.selectedData}
+                            onChange={this.handleChange}
+                        >
+                            <MenuItem value={null}><em>Select Country</em></MenuItem>
+                            {this.state.data.map((entity, i) =>
+                                (<MenuItem key={i} value={entity}>{entity.country}</MenuItem>
+                                ))}
+                        </Select>
+                    </FormControl>
+                </div>
+
+                <div className={classes.body}>
+                    {this.state.selectedData === null ?
+                        <Typography align="center">
+                            Please select a country to view statistic
+                    </Typography>
+                        :
+
+                        <Grid container spacing={2}>
+
+                            <Grid item xs={12} sm={12}>
+                                <Typography align="center" variant="h4">
+                                    <p> {this.state.selectedData.country}</p>
+                                </Typography>
+                                <p align="center"> As of {this.state.selectedData.day}</p>
+                            </Grid>
+
+                            <Grid item xs={12} sm={3}>
+                                <Typography align="center" variant="h5">
+                                    Active Cases
+                        </Typography>
+                                <Typography align="center" variant="h6" >
+                                    <p> {this.state.selectedData.cases.active} ({this.state.selectedData.cases.new} new cases)</p>
+                                </Typography>
+                            </Grid>
+
+                            <Grid item xs={12} sm={3}>
+                                <Typography align="center" variant="h5">
+                                    Critical
+                                </Typography>
+                                <Typography align="center" variant="h6" className={classes.mainColor}>
+                                    <p className={classes.critical}> {this.state.selectedData.cases.critical === null ? 0 : this.state.selectedData.cases.critical}</p>
+                                </Typography>
+                            </Grid>
+                            
+                            <Grid item xs={12} sm={3}>
+                                <Typography align="center" variant="h5">
+                                    Recovered
+                                </Typography>
+                                <Typography align="center" variant="h6" className={classes.mainColor}>
+                                    <p> {this.state.selectedData.cases.recovered}</p>
+                                </Typography>
+                            </Grid>
+
+                            <Grid item xs={12} sm={3}>
+                                <Typography align="center" variant="h5">
+                                    Total Deaths
+                                </Typography>
+                                <Typography align="center" variant="h6" className={classes.mainColor}>
+                                    <p className={classes.critical}> {this.state.selectedData.deaths.total === null ? 0 : this.state.selectedData.cases.critical}</p>
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    }
+                </div>
+            </React.Fragment >
         )
     }
 }
+
+const styles = (theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    body: {
+        margin: theme.spacing(1),
+    },
+    critical: {
+        color: "#f70008"
+    }
+});
+
+export default withStyles(styles)(Statistic);
