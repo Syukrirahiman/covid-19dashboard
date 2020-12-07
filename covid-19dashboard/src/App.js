ba72e5f79d81b7b6fd785b59cd917cf55ea058a1
Syukrirahiman <43146706+Syukrirahiman@users.noreply.github.com>
2020-12-07 16:40:22 +0800

    Added Header Navbar

--- covid-19dashboard/src/App.js
+++ covid-19dashboard/src/App.js
@@ -1,12 +1,30 @@
+import React, { Component } from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import 'assets/css/App.css';
-import StatisticView from 'view/StatisticView/StatisticView.js'
 
-function App() {
-  return (
-    <div className="App">
-      <StatisticView/>
-    </div>
-  );
+import theme from "theme";
+import Navbar from 'view/Navbar'
+import StatisticView from 'view/StatisticView/StatisticView'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <StatisticView/>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+        </Router>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default App;
